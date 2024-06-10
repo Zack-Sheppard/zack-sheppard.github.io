bae439042efc2fe4921a9993483147acdf8d8b1a
Zack Sheppard <zack.sheppard@gmail.com>
2024-06-10 09:55:00 -0600

    site version alpha

--- scripts.js
+++ scripts.js
@@ -1,30 +1,69 @@
 
-// you sneaky bastard
+const ACTIVE_COLOUR   = "#0FF"; // isn't it ironic
+const INACTIVE_COLOUR = "#474747";
 
-const paragraphs = ["self", "software", "education", "music"];
-
-function toggleSoftware() {
-    let x = document.getElementById("software");
-    let y = document.getElementById("software-link");
-    if (x.style.display == "none" || x.style.display == "") {
-        x.style.display = "block";
-        y.style.color = "#000000";
+const categories = [
+    { 
+        title: "software",
+        visible: false
+    },
+    { 
+        title: "education",
+        visible: false
+    },
+    { 
+        title: "music",
+        visible: false
     }
-    else if(x.style.display == "block") {
-        // embolden the header text
-        x.style.display = "none";
-        y.style.color = "#333333";
+];
+
+function toggleCategory(category) {
+    categories.forEach((el, i) => {
+        if(el.title == category) {
+            categories[i].visible = !el.visible;
+            let link = document.getElementById(category + "-link");
+            link.style.color = categories[i].visible ? ACTIVE_COLOUR : INACTIVE_COLOUR;
+        }
+    });
+}
+
+function hideAllParagraphs() {
+    // Trust me, this was easier
+    document.getElementById("self").style.display = "none";
+    document.getElementById("software").style.display = "none";
+    document.getElementById("education").style.display = "none";
+    document.getElementById("music").style.display = "none";
+    document.getElementById("software-education").style.display = "none";
+    document.getElementById("software-music").style.display = "none";
+    document.getElementById("education-music").style.display = "none";
+    document.getElementById("software-education-music").style.display = "none";
+}
+
+// you sneaky bastard
+function displayCorrectParagraph() {
+
+    let paragraph = categories.reduce((acc, curr) => {
+        if(curr.visible && acc == "") {
+            return acc + curr.title;
+        }
+        else if(curr.visible) {
+            return acc + "-" + curr.title;
+        }
+        else {
+            return acc;
+        }
+    }, "");
+
+    if(paragraph == "") {
+        document.getElementById("self").style.display = "block";
     }
     else {
-        console.log("something went wrong");
-        console.log(x.style.display);
+        document.getElementById(paragraph).style.display = "block";
     }
 }
 
-function toggleEducation() {
-    // TODO
-}
-
-function toggleMusic() {
-    // TODO
+function changeView(category) {
+    toggleCategory(category);
+    hideAllParagraphs();
+    displayCorrectParagraph();
 }
